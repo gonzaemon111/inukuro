578acc1dd40b8068a8d60e09ebedb9f8592ec606
gonzaemon111 <goki14914@icloud.com>
2019-09-14 20:14:08 +0900

    init commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-brand">
+                <nuxt-link to="/breeds" class="navbar-item">イヌコロ</nuxt-link>
+                <img src="~/assets/18nine.png">
+                <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+            </div>
+        </div>
+    </nav>
+</template>
